6d821998b62792dc31aefe7fb778d77ba6a93463
RevolveStrm <revolvebattlerage@gmail.com>
2024-12-16 19:47:23 +0200

    #5: add database connect handling

--- src/database/database-service.ts
+++ src/database/database-service.ts
@@ -1,3 +1,13 @@
 import { PrismaClient } from "@prisma/client";
+import { loggerService } from "../logger/logger-service";
 
 export const databaseService = new PrismaClient();
+
+databaseService
+	.$connect()
+	.then(() => loggerService.info("Prisma successfully connected"))
+	.catch((err) => {
+		console.error(err);
+		loggerService.error(`Prisma connection failed: ${err}`);
+		process.exit(1);
+	});
